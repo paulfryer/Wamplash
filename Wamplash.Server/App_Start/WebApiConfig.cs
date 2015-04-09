5d9b8eb69cbc8902751831ad486f811dae61f9a5
Paul Fryer <paulfryer@macrowebservices.com>
2015-04-09 10:26:53 -0700

    working on server.

--- Wamplash.Server/App_Start/WebApiConfig.cs
+++ Wamplash.Server/App_Start/WebApiConfig.cs
@@ -20,6 +20,7 @@ namespace Wamplash.Server
             // Web API routes
             //config.MapHttpAttributeRoutes();
 
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
