27ccf2b9b0fe54f87dcbcbea3b764959b9631541
Paul Fryer <paulfryer@macrowebservices.com>
2015-04-09 10:28:56 -0700

    Got redis demo working.

--- Wamplash.Server/App_Start/WebApiConfig.cs
+++ Wamplash.Server/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace Wamplash.Server
            // config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
-            //config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes();
 
             
             config.Routes.MapHttpRoute(
