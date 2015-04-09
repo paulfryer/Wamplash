1e9b3246015cce1cccbde6d9b4f232048e30072b
Paul Fryer <paulfryer@macrowebservices.com>
2015-04-09 10:15:44 -0700

    adding missing files

---
+++ Wamplash.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wamplash.Server.WebApiApplication" Language="C#" %>
--- Wamplash.Server/Global.asax.cs
+++ Wamplash.Server/Global.asax.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
+using System.Web.Optimization;  
 using System.Web.Routing;
 using Autofac;
 using Autofac.Integration.Mvc;
